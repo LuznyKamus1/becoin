f7b9d309a57a984d7864ad6c720c065bdd029bfc
kamus <r3kinek0@gmail.com>
2022-10-25 23:01:58 +0200

    slight improvements

--- main.py
+++ main.py
@@ -1,83 +1,67 @@
-#import libs
-import json 
-import random 
+import json
 import os 
 import discord
 from discord.ext import commands
 
-#define vars
-ofile = open("../walletjson/info.json","r+",encoding='utf-8')
-info = json.load(ofile)
+jfile = open("wallets.json", "r+", encoding='utf-8')
+info = json.load(jfile)
 
-#define commands prefix
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
 
-#test if bot responses
+print("running on: ", info["token"])
+
 @bot.command()
 async def test(ctx):
-    print("TEST")
+    print("test")
     await ctx.send('test')
 
-#check how many becoin does user have
 @bot.command()
-async def stats(ctx):
-    print("STATS")
-    try:
-        status=info["accounts"][str(ctx.message.author.id)]["balance"]
+async def acc(ctx):
+    print("acc")
+    #commands.UserConverter("481159373620510731")
+    await ctx.send(info["wallets"][str(ctx.author.id)])
 
-        if((status)<100):
-            await ctx.message.author.send("you are poor ("+str(status)+" becoins)")
-        else:
-            await ctx.message.author.send("you have "+str(status)+ " becoins")
-    except json.decoder.JSONDecodeError:
-        print("There was a problem accessing the json data (balance).")
-    await ctx.message.delete()
-
-#give specified amount of money to specified user id
 @bot.command()
-async def give(ctx, user:discord.User, ammount):
-    print("GIVE")
+async def give(ctx, user:discord.User, amnt:float):
+    print("give")
     try:
-        if((info["accounts"][str(ctx.message.author.id)]["balance"]-float(ammount))<0):
-            await ctx.send("you have insufficient ammount of becoins")
+        if not(str(user.id) == str(ctx.author.id)):
+            if not((info["wallets"][str(ctx.author.id)]-amnt)<0):
+                info["wallets"][str(ctx.author.id)]-=-amnt
+                info["wallets"][str(user.id)]+=amnt
+                await ctx.send("sent "+str(amnt)+" to "+str(user))
+            else:
+                await ctx.send("imagine")
         else:
-            info["accounts"][str(user.id)]["balance"] += float(ammount)
-            info["accounts"][str(ctx.message.author.id)]["balance"] -= float(ammount)
-            await ctx.message.author.send("sent " + str(ammount) + " balance" + " to " + str(user))
-
-
+                await ctx.send("imagine")
     except ValueError:
-        await ctx.send('wrong ammount')
-    await ctx.message.delete()
+        await ctx.send("wrong ammount")
+    except commands.errors.UserNotFound:
+        await ctx.send("wrong user")
+    except commands.errors.CommandInvokeError:
+        await ctx.send("user not registered")
+            
+@bot.command()
+async def bankgive(ctx, user:discord.User, amnt:float):
+    print("bankgive")
+    if(discord.utils.get(ctx.guild.roles, name='zaufany') in ctx.author.roles):
+        info["wallets"][str(user.id)]+=float(amnt)
+        print("    bankgave ", amnt, " to", str(user))
+    else: await ctx.send("you cannot bankgive")
 
 @bot.command()
-async def helpme(ctx):
-    print("HELPME")
-    await ctx.send("!test                    - bot says test")
-    await ctx.send("!stats                   - see ho much money do you hav")
-    await ctx.send("!give `user id` `amount` - gives specified amount of money from your account to specified user")
-    await ctx.send("for further help contact - apul ajfon#3144")
-    await ctx.message.delete()
+async def register(ctx):
+    print("register")
+    y={str(ctx.author.id):1000}
+    info["wallets"].update(y)
+    print(json.dumps(info))
+    
 
-# @bot.command()
-# async def register(ctx):
-#     print("REGISTER")
-#     if(str(ctx.message.author.id) in info["accounts"]):
-#         info["accounts"][str(ctx.message.author.id)]
+bot.run(info["token"])
 
-@bot.command()
-async def bankgive(ctx, user:discord.User, ammount):
-    print("BANKGIVE")
-    if(discord.utils.get(ctx.guild.roles, name="Bankier") in ctx.message.author.roles):
-        info["accounts"][str(user.id)]["balance"] += float(ammount)
-        print("    good")
-
-    else: print("    bad")
-    await ctx.message.delete()
-
-bot.run(info["discordinfo"]["token"])
-#save file on exit and close
-ofile.seek(0)
-ofile.truncate()
-json.dump(info, ofile)
-ofile.close()
+jfile.seek(0)
+jfile.truncate()
+json.dump(info, jfile)
+jfile.close()
